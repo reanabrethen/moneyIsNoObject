2f42fc28ce5be9dbcf3faa3565bc1f90b98f3471
Admin <reanajb@gmail.com>
2023-12-19 13:49:08 -0500

    completed homework

--- main.js
+++ main.js
@@ -3,8 +3,53 @@
 // └─────────────────┘
 
 
+function isAdmin(obj){
+   if(obj['userRole'] === "ADMIN"){
+    return true
+}else{
+    return false
+}
+}
+//returns boolean --> if statements
+
+function getEmail(obj){
+    return obj.firstName[0].toLowerCase() + obj.lastName.toLowerCase() + ".prsvr@gmail.com"
+}
 
 
+function getPlaylistLength(obj){
+    return obj['songs'].length 
+}
+
+
+function getHardestHomework(obj){
+    if(obj.length === 0){
+        return ""
+    }
+
+    let homework = obj[0].name;
+    let scoreAvg = obj[0].averageScore
+    for(let i = 0; i < obj.length; i++){
+        let complete = obj[i]
+        complete.averageScore
+        if(complete.averageScore < scoreAvg){
+            homework = complete.name
+            scoreAvg = complete.averageScore
+        }
+    }return homework
+}
+
+
+function createPhonebook(name, number){
+    let phonebook = {}
+    for(let i = 0; i < name.length; i++){
+       phonebook[name[i]] = number[i]
+    }
+    return phonebook
+}
+
+//creating object from arrays
+//pull values from array to create object
 
 // ┌─────────────────────────────────────┐
 // │ Do not modify code below this line. │
